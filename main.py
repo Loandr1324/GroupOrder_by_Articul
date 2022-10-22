7ab3d3ebc4113cb02705b95c796dfc0853155164
Loik Andrey <7034@balancedv.ru>
2022-10-22 12:38:51 +1100

    1. Добавил описание функций. 2. Вынес форматы экселя в отдельную функцию format_exel().

--- main.py
+++ main.py
@@ -4,25 +4,27 @@ import os
 from loguru import logger
 
 
-"""logger.add(config.FILE_NAME_CONFIG,
-           format="{time:DD/MM/YY HH:mm:ss} - {file} - {level} - {message}",
-           level="INFO",
-           rotation="1 month",
-           compression="zip")"""
-
-
 def search_file():
+    """
+    Ищем первый файл с расширением 'xls'
+
+    :return: str с наименованием файла
+    """
     for item in os.listdir():
         if item.endswith('xls'):
-            logger.info(f"Find file 'xls': {item}")
+            logger.info(f"Нашёл файл с расширением 'xls': {item}")
             return item
-    logger.error(f"Can't find file 'xls'")
-
-def read_file(file):
-    return pd.read_excel(file)
+    logger.error(f"Не смог найти файл с расширением 'xls'")
 
 
 def rebuild_df(df):
+    """
+    Очищаем данные из полученного DataFrame счёта и возвращаем данные заголовка и данные таблицы
+
+    :param df: Dataframe с данными счёта
+
+    :return: df -> DataFrame с данными по товарам, df_head -> DataFrame со строкой заголовка
+    """
     mask_end = df == 'Итого за вес'
     end_row = df[mask_end].dropna(axis=0, how='all').index.values
     mask_start = df == 'Марка'
@@ -40,6 +42,13 @@ def rebuild_df(df):
 
 
 def groupby_df(df):
+    """
+    Группируем строки таблицы по колонкам 'Номер', 'Описание', 'Вес детали'
+
+    :param df: DataFrame с позициями счёта
+
+    :return: df -> DataFrame с группирпованными данными по колонкам 'Номер', 'Описание', 'Вес детали'
+    """
     df = df.reset_index(drop=True)
     if 'Замена' in df.columns:
         df = df.groupby(['Номер', 'Описание', 'Вес детали'], as_index=False).agg({
@@ -65,6 +74,13 @@ def groupby_df(df):
 
 
 def sort_df(df):
+    """Переставляем местами колонки
+
+    :param: DataFrame с наименование колонок Марка', 'Номер', 'Reference', 'Описание',
+                 'Кол-во', 'Цена RUB', 'Сумма RUB', 'Вес детали', 'Общий вес' так же может быть колонка
+                 с наименованием 'Замена'
+
+    :return: DataFrame с отсортированными колонками"""
     if 'Замена' in df.columns:
         df = df[['Замена', 'Марка', 'Номер', 'Reference', 'Описание',
                  'Кол-во', 'Цена RUB', 'Сумма RUB', 'Вес детали', 'Общий вес']]
@@ -75,52 +91,22 @@ def sort_df(df):
 
 
 def final_scores(df):
+    """Рассчитываем итоговые значения по таблице
+
+    :param: DataFrame с колонками 'Сумма RUB' 'Общий вес'
+
+    :return: (total_sum , total_weight, cost_delivery)
+    """
     total_sum = round(df['Сумма RUB'].sum(), 2)
     total_weight = round(df['Общий вес'].sum(), 3)
     cost_delivery = total_weight * 200
     return total_sum, total_weight, cost_delivery
 
 
-def df_to_excel(df, header, file_name):
+def format_exel():
+    """Задём форматы для таблицы эксель
 
-    start_row_table = 4
-    # Получаем итоговые значения
-    logger.info('Считаем итоговые значения')
-    total_sum, total_weight, cost_delivery = final_scores(df)
-
-    # Сбрасываем встроенный формат заголовков pandas
-    pd.io.formats.excel.ExcelFormatter.header_style = None
-
-    # Открываем файл для записи
-    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
-    sheet_name = 'Sheet1'  # Задаём имя вкладки
-    workbook = writer.book  # Открываем книгу для записи
-
-    # Записываем данные на вкладку sheet_name
-    header.to_excel(writer, sheet_name=sheet_name, index=False, header=False, startrow=1)
-    df.to_excel(writer, sheet_name=sheet_name, index=False, startrow=start_row_table)
-
-    # Выбираем вкладку для форматирования
-    wks1 = writer.sheets[sheet_name]
-
-    header_format = workbook.add_format({
-        'font_name': 'Arial',
-        'font_size': '7',
-        'align': 'center',
-        'valign': 'top',
-        'text_wrap': True,
-        'bold': True,
-        'bg_color': '#F4ECC5',
-        'border': True,
-        'border_color': '#CCC085'
-    })
-    # Форматируем таблицу
-    wks1.set_default_row(12)
-    wks1.set_row(start_row_table, 20, header_format)
-    # wks1.set_column('A:A', 12, name_format)
-    # wks1.set_column('B:B', 32, name_format)
-    # wks1.set_column('C:H', 10, data_format)
-    # wks1.set_column('I:I', 12, data_format)
+    :return: header_format, name_format, name_format_rigth, header_format, footer_format"""
     name_format = workbook.add_format({
         'font_name': 'Arial',
         'font_size': '8',
@@ -153,46 +139,95 @@ def df_to_excel(df, header, file_name):
         'font_name': 'Arial',
         'valign': 'right'
     })
-    wks1.set_column('A:F', 10, name_format)
-    wks1.set_column('F4:J100', 10, name_format_rigth)
+    return name_format, name_format_rigth, header_format, footer_format
 
-    end_table = start_row_table + len(df) + 1
+
+def df_to_excel(df, header, file_name):
+    """Создаём и форматируем файл эксель
+
+    :param DataFrame - таблица с данными, DataFrame - с заголовком, filename -> str - наименование файла
+
+    :return None"""
+
+    # Определяем номер строки начала и конца таблицы
+    start_row_table = 4
+    end_row_table = start_row_table + len(df) + 1
+
+    # Получаем итоговые значения
+    logger.info('Считаем итоговые значения')
+    total_sum, total_weight, cost_delivery = final_scores(df)
+
+    # Сбрасываем встроенный формат заголовков pandas
+    pd.io.formats.excel.ExcelFormatter.header_style = None
+
+    # Открываем файл для записи
+    writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+    sheet_name = 'Sheet1'  # Задаём имя вкладки
+    workbook = writer.book  # Открываем книгу для записи
+
+    # Записываем данные на вкладку sheet_name
+    header.to_excel(writer, sheet_name=sheet_name, index=False, header=False, startrow=1)
+    df.to_excel(writer, sheet_name=sheet_name, index=False, startrow=start_row_table)
+
+    # Получаем форматы
+    table_format, table_format_rigth, header_format, footer_format = format_exel()
+
+    # Выбираем вкладку для форматирования
+    wks1 = writer.sheets[sheet_name]
+
+    # Форматируем таблицу
+    wks1.set_default_row(12) # Высота строк по умолчанию
+    wks1.set_row(start_row_table, 20, header_format) # Формат и высота шапки таблицы
+
+    # Формат всех колонок
+    wks1.set_column('A:E', 10, table_format)
+    wks1.set_column('F:J', 10, table_format_rigth)
 
     # Записываем итоговые данные
-    wks1.write(f'F{end_table + 2}', 'Итого:', None)
-    wks1.write(f'G{end_table + 2}', total_sum, None)
-    wks1.write(f'F{end_table + 4}', 'Итого за вес:', None)
-    wks1.write(f'G{end_table + 4}', cost_delivery, None)
-    wks1.write(f'H{end_table + 2}', 'Итого вес:', None)
-    wks1.write(f'I{end_table + 2}', total_weight, None)
+    wks1.write(f'F{end_row_table + 2}', 'Итого:', None)
+    wks1.write(f'G{end_row_table + 2}', total_sum, None)
+    wks1.write(f'F{end_row_table + 4}', 'Итого за вес:', None)
+    wks1.write(f'G{end_row_table + 4}', cost_delivery, None)
+    wks1.write(f'H{end_row_table + 2}', 'Итого вес:', None)
+    wks1.write(f'I{end_row_table + 2}', total_weight, None)
 
     # Изменяем формат строк заголовка
     for i in range(0, start_row_table):
         wks1.set_row(i, 16, header_format)
-    # Изменяем формат строк подвала таблицы
-    for i in range(end_table, end_table + 200):
+    for i in range(end_row_table, end_row_table + 50):
         wks1.set_row(i, 16, footer_format)
 
-# Сохраняем файл
+    # Сохраняем файл
     writer.save()
     return
 
 
-
 def run():
+    """
+    Основной код программы
+
+    :return: None
+    """
+
     logger.info('Получаем имя файла')
     filename = search_file()
+
     logger.info('Считываем файл в память')
-    df = read_file(filename)
+    df = pd.read_excel(filename)
+
     logger.info('Перестраиваем таблицу')
     df, df_header = rebuild_df(df)
+
     logger.info('Группируем значения по Артикулу')
     df = groupby_df(df)
+
     logger.info('Подготавливаем таблицу для записи')
     df = sort_df(df)
+
     logger.info('Сохраняем в файл')
     new_filename = f'_{filename[:-4]}_.xlsx'
     df_to_excel(df, df_header, new_filename)
+
     logger.info('Программа завершила свою работу')
 
 
